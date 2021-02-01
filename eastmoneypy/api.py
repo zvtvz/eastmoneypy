45739a7dbf82259fd7aed4a92e8ba7e8fce71647
foolcage <5533061@qq.com>
2021-02-01 19:01:21 +0800

    update with latest api

--- eastmoneypy/api.py
+++ eastmoneypy/api.py
@@ -1,5 +1,6 @@
 import logging
 
+import demjson
 import requests
 from requests import Response
 
@@ -26,8 +27,10 @@ def chrome_copy_header_to_dict(src):
 
 HEADER = chrome_copy_header_to_dict(my_env['header'])
 
+APIKEY = my_env['appkey']
 
-def parse_resp(resp: Response):
+
+def parse_resp(resp: Response, key=None):
     if resp.status_code != 200:
         raise Exception(f'code:{resp.status_code},msg:{resp.content}')
     # {
@@ -35,59 +38,66 @@ def parse_resp(resp: Response):
     #   "message": "",
     #   "result": {}
     # }
-    ret = resp.json()
+    result = resp.text
+    js_text = result[result.index('(') + 1:result.index(')')]
+
+    ret = demjson.decode(js_text)
     logger.info(f'ret:{ret}')
-    return ret['re'], ret.get('result')
+    data = ret.get('data')
+    if data and key:
+        result_value = data.get(key)
+    else:
+        result_value = data
+
+    return ret['state'], result_value
 
 
 def create_group(group_name):
-    url = 'http://quote.eastmoney.com/zixuan/api/zxg/addgroup'
-    resp = requests.post(url, headers=HEADER,
-                         data={'groupname': group_name})
+    url = f'http://myfavor.eastmoney.com/v4/webouter/ag?appkey={APIKEY}&cb=jQuery112404771026622113468_1612176493845&gn={group_name}&_=1612176493849'
+    resp = requests.get(url, headers=HEADER)
 
-    return parse_resp(resp)
+    _, group = parse_resp(resp)
+    return group
 
 
 def get_groups():
-    url = 'http://quote.eastmoney.com/zixuan/api/zxg/group'
+    url = f'http://myfavor.eastmoney.com/v4/webouter/ggdefstkindexinfos?appkey={APIKEY}&cb=jQuery112407703233916827181_1612173986286&g=1&_=1612173986288'
 
     resp = requests.get(url, headers=HEADER)
 
-    _, value = parse_resp(resp)
-    if value:
-        return value['groups']
+    _, value = parse_resp(resp, key='ginfolist')
+    return value
 
 
 def rename_group(group_id, group_name):
-    url = 'http://quote.eastmoney.com/zixuan/api/zxg/editgroupname'
+    url = f'http://myfavor.eastmoney.com/v4/webouter/mg?appkey={APIKEY}&cb=jQuery112406922055532444666_1612177151715&g={group_id}&gn={group_name}&_=1612177151728'
 
-    resp = requests.post(url, headers=HEADER,
-                         data={'groupid': group_id, 'groupname': group_name})
+    resp = requests.get(url, headers=HEADER)
 
     ret, _ = parse_resp(resp)
     return ret
 
 
 def del_group(group_name=None, group_id=None):
-    url = 'http://quote.eastmoney.com/zixuan/api/zxg/deletegroup'
     if not group_id:
         assert group_name is not None
         group_id = get_group_id(group_name)
         if not group_id:
             raise Exception(f'could not find group:{group_name}')
 
-    resp = requests.post(url, headers=HEADER,
-                         data={'groupid': group_id})
+    url = f'http://myfavor.eastmoney.com/v4/webouter/dg?appkey={APIKEY}&cb=jQuery1124005355240135242356_1612173048874&g={group_id}&_=1612173048950'
 
-    ret, _ = parse_resp(resp)
+    resp = requests.get(url, headers=HEADER)
+
+    ret, _ = parse_resp(resp, key=None)
     return ret
 
 
 def get_group_id(group_name):
     groups = get_groups()
-    groups = [group for group in groups if group['name'] == group_name]
+    groups = [group for group in groups if group['gname'] == group_name]
     if groups:
-        return groups[0]['id']
+        return groups[0]['gid']
     return None
 
 
@@ -97,30 +107,34 @@ def add_to_group(code, entity_type='stock', group_name=None, group_id=None):
         group_id = get_group_id(group_name)
         if not group_id:
             raise Exception(f'could not find group:{group_name}')
-    url = 'http://quote.eastmoney.com/zixuan/api/zxg/addstock'
-    resp = requests.post(url, headers=HEADER,
-                         data={'groupid': group_id, 'stockcode': to_eastmoney_code(code=code, entity_type=entity_type)})
+    code = to_eastmoney_code(code, entity_type=entity_type)
+    url = f'http://myfavor.eastmoney.com/v4/webouter/as?appkey={APIKEY}&cb=jQuery112404771026622113468_1612176493843&g={group_id}&sc={code}&_=1612176493913'
+    resp = requests.get(url, headers=HEADER)
 
-    ret, _ = parse_resp(resp)
-    return ret
+    return parse_resp(resp)
 
 
 def to_eastmoney_code(code, entity_type='stock'):
     if entity_type == 'stock':
         # 上海
         if code >= '333333':
-            return f'1.{code}'
+            return f'1%24{code}'
         else:
-            return f'0.{code}'
+            return f'0%24{code}'
 
 
 __all__ = ['create_group', 'get_groups', 'rename_group', 'del_group', 'add_to_group', 'to_eastmoney_code']
 
 if __name__ == '__main__':
     groups = get_groups()
-    if len(groups) >= 9:
-        del_group(group_id=groups[-2]['id'])
+    print(groups)
 
-    create_group('tmp')
-    add_to_group('000888', group_name='tmp')
-    del_group('tmp')
+    print(get_group_id('bull'))
+
+    del_group('bull')
+
+    g = create_group('bull')
+    print(g)
+
+    print(add_to_group('000999', group_name='bull'))
+    print(add_to_group('688001', group_name='bull'))
